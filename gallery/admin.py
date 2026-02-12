389d3e94ca499943009e389d215c86a1f5e1551f
Steve <steve@example.com>
2026-02-12 19:18:05 +0530

    Fix media serving in production

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,9 +1,30 @@
 from django.contrib import admin
+from django import forms
 from .models import GalleryImage
 
-@admin.register(GalleryImage)
-class GalleryImageAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "active", "created_at")
-    list_filter = ("active",)
-    search_fields = ("title",)
 
+class GalleryImageAdminForm(forms.ModelForm):
+    images = forms.ImageField(
+        widget=forms.ClearableFileInput(attrs={'multiple': True}),
+        required=False
+    )
+
+    class Meta:
+        model = GalleryImage
+        fields = "__all__"
+
+
+class GalleryImageAdmin(admin.ModelAdmin):
+    form = GalleryImageAdminForm
+
+    def save_model(self, request, obj, form, change):
+        images = request.FILES.getlist("images")
+
+        if images:
+            for img in images:
+                GalleryImage.objects.create(image=img)
+        else:
+            super().save_model(request, obj, form, change)
+
+
+admin.site.register(GalleryImage, GalleryImageAdmin)
--- turf_booking/urls.py
+++ turf_booking/urls.py
@@ -1,5 +1,3 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,5 +7,5 @@ urlpatterns = [
     path("gallery/", include("gallery.urls")),
 ]
 
-# 🔥 Always serve media (for Render)
+# 🔥 ALWAYS serve media (for now)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
